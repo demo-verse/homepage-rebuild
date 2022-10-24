b8dbfeab6f6ea8331aa121349ee3ac65fa4b3934
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-10-24 23:46:53 +0300

    update flow of peace with miro link, focused on that view

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -41,7 +41,7 @@ function Navbar({ button }) {
           <a href="https://discord.gg/sqjdyCWNAR" target={"_blank"} rel="noreferrer">A New human right</a>
           </li> */}
           <li>
-          <a href="https://i.imgur.com/ToUaIlL.png" target={"_blank"} rel="noreferrer">A flow of peace</a>
+          <a href="https://miro.com/app/board/uXjVOX94E5k=/?share_link_id=267054985896" target={"_blank"} rel="noreferrer">A flow of peace</a>
           </li> 
           <li>
           <a href="https://docs.google.com/document/d/1sWWbXOJSf5UdsJr79uQChGEDaFI9cdI-5KSFCARqwoE/" target={"_blank"} rel="noreferrer">Short paper</a>
